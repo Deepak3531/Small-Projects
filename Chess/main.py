3678440d18e24805ad3d2fb885f1db14d8b046fd
Deepak3531 <deepakhadale5@gmail.com>
2024-12-07 17:27:47 +0200

    creating chess game with pygame

---
+++ Chess/main.py
@@ -0,0 +1,30 @@
+# two player of chess game in python with pygame!
+# set up variable, images and game loop
+
+import pygame
+
+pygame.init()
+width = 1000
+height = 900
+screen = pygame.display.set_mode([width, height])
+font = pygame.font.Font('freesansbold.ttf', 20)
+big_font = pygame.font.Font('freesansbold.ttf', 50)
+timer = pygame.time.Clock()
+fps = 60
+# game variable and images
+
+
+# game loop
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('dark grey')
+    
+    # event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    pygame.display.flip()
+pygame.quit()
+        
